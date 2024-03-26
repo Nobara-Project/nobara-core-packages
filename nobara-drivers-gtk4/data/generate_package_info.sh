659a9c7ca092100e846f164de59b6bd2d814f34f
GloriousEggroll <gloriouseggroll@gmail.com>
2024-03-25 23:24:20 -0600

    drivers dnf command fixup

--- nobara-drivers-gtk4/data/generate_package_info.sh
+++ nobara-drivers-gtk4/data/generate_package_info.sh
@@ -3,9 +3,9 @@
 if [[ $1 == "version" ]]
 then
 	#apt-cache show $2 | grep Version: | cut -d":" -f2 | head -n1
-	echo "GE PUT A COMMAND HERE THAT GETS THE VERSION OF A DNF PACKAGE (WITHOUT ROOT)"
+	dnf info available $2 | grep Version | cut -d":" -f2 | head -n1
 elif [[ $1 == "description" ]]
 then
 	#apt-cache show $2 | grep 'Description*' | cut -d":" -f2 | head -n1
-	echo "GE PUT A COMMAND HERE THAT GETS THE DESCRIPTION OF A DNF PACKAGE (WITHOUT ROOT)"
+	dnf info available $2 | grep Description | cut -d":" -f2 | head -n1
 fi
