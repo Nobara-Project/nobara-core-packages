01a6fec4ee68d3ea19f9f2600a5fae94bc434c67
matte-schwartz <136293710+matte-schwartz@users.noreply.github.com>
2024-04-01 19:59:44 -0700

    add new packages to tarball script

--- create-tarballs.sh
+++ create-tarballs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 tar -cvzf kde-nobara.tar.gz kde-nobara
+tar -cvzf nobara-drivers-gtk4.tar.gz nobara-drivers-gtk4
 tar -cvzf nobara-nvidia-wizard.tar.gz nobara-nvidia-wizard
 tar -cvzf nobara-resolve-runtime.tar.gz nobara-resolve-runtime
 tar -cvzf nobara-logos.tar.gz nobara-logos
+tar -cvzf nobara-welcome-gtk4.tar.gz nobara-welcome-gtk4
 
 cd nobara-welcome/38
 tar -cvzf nobara-welcome-38.tar.gz nobara-welcome
