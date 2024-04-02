9d0179bea07092093a591e0bf6cc87fb7474b6b3
matte-schwartz <136293710+matte-schwartz@users.noreply.github.com>
2024-04-01 18:38:49 -0700

    use dnf info instead of dnf info available
    this gets around an issue preventing fedora repo packages from showing a description once installed to the system. otherwise, dnf info available $2 returns:
    
    Error: No matching Packages to list

--- nobara-drivers-gtk4/data/generate_package_info.sh
+++ nobara-drivers-gtk4/data/generate_package_info.sh
@@ -3,9 +3,9 @@
 if [[ $1 == "version" ]]
 then
 	#apt-cache show $2 | grep Version: | cut -d":" -f2 | head -n1
-	dnf info available $2 | grep Version | cut -d":" -f2 | head -n1
+	dnf info $2 | grep Version | cut -d":" -f2 | head -n1
 elif [[ $1 == "description" ]]
 then
 	#apt-cache show $2 | grep 'Description*' | cut -d":" -f2 | head -n1
-	dnf info available $2 | grep Description | cut -d":" -f2 | head -n1
+	dnf info $2 | grep Description | cut -d":" -f2 | head -n1
 fi
