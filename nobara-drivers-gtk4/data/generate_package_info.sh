aa1c8b2a5082600cc713d66fac065dd7533e7e4e
Matthew Schwartz <njtransit215@gmail.com>
2024-04-04 19:30:57 -0700

    add new entries to driver-db

--- nobara-drivers-gtk4/data/generate_package_info.sh
+++ nobara-drivers-gtk4/data/generate_package_info.sh
@@ -7,5 +7,5 @@ then
 elif [[ $1 == "description" ]]
 then
 	#apt-cache show $2 | grep 'Description*' | cut -d":" -f2 | head -n1
-	dnf info $2 | grep Description | cut -d":" -f2 | head -n1
+	dnf info $2 | sed -n '/Description/,/^$/p' | awk 'NR==1,/^$/ {if (/^$/ && printed) exit; if (NF) printed=1; if (NR==1) sub(/Description *: */, ""); else sub(/^ *: */, ""); print}'
 fi
