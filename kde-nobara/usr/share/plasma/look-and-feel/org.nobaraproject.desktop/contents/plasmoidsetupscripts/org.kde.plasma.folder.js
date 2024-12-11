9eeda67ceb481819846747fa869fb123aa1e1860
GloriousEggroll <gloriouseggroll@gmail.com>
2024-12-11 00:01:17 -0500

    add N41 wallpapers to kde-nobara

--- kde-nobara/usr/share/plasma/look-and-feel/org.nobaraproject.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
+++ kde-nobara/usr/share/plasma/look-and-feel/org.nobaraproject.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -1,4 +1,4 @@
 applet.wallpaperPlugin = 'org.kde.image'
 applet.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"]
-applet.writeConfig("Image", "/usr/share/wallpapers/nobara-40-2.png")
+applet.writeConfig("Image", "/usr/share/wallpapers/nobara-41-1.png")
 applet.reloadConfig()
