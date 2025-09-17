e354f29a40224344c417a1898dcf266700110165
GloriousEggroll <gloriouseggroll@gmail.com>
2025-09-17 02:42:58 -0600

    bump create tarballs script

--- create-tarballs.sh
+++ create-tarballs.sh
@@ -5,3 +5,5 @@ tar -cvzf nobara-resolve-runtime.tar.gz nobara-resolve-runtime
 tar -cvzf nobara-logos.tar.gz nobara-logos
 tar -cvzf nobara-welcome-gtk4.tar.gz nobara-welcome-gtk4
 tar -cvzf nobara-updater.tar.gz nobara-updater
+tar -cvzf nobara-automount.tar.gz nobara-automount
+tar -cvzf nobara-resolve-wizard.tar.gz nobara-resolve-wizard
