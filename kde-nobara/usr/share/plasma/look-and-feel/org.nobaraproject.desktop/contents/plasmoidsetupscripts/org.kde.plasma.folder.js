63a9a679bc09b6418bb6b18d4c09a3dcbe1f2e24
GloriousEggroll <gloriouseggroll@gmail.com>
2026-08-22 00:55:37 -0600

    kde-nobara: replace ai generated wallpapers with community ones :)

--- kde-nobara/usr/share/plasma/look-and-feel/org.nobaraproject.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
+++ kde-nobara/usr/share/plasma/look-and-feel/org.nobaraproject.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -1,4 +1,4 @@
 applet.wallpaperPlugin = 'org.kde.image'
 applet.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"]
-applet.writeConfig("Image", "/usr/share/wallpapers/nobara-42-1.png")
+applet.writeConfig("Image", "/usr/share/wallpapers/nobara-winds1.webp")
 applet.reloadConfig()
